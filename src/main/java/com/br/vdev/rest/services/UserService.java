f3705b7fcb67ed3187fe342c152a41df06618a60
VictorW-dev <victorwinicius.bs@gmail.com>
2021-06-20 00:49:01 -0300

    Getting a user by ID

--- src/main/java/com/br/vdev/rest/services/UserService.java
+++ src/main/java/com/br/vdev/rest/services/UserService.java
@@ -1,12 +1,14 @@
 package com.br.vdev.rest.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.br.vdev.rest.domain.User;
 import com.br.vdev.rest.repository.UserRepository;
+import com.br.vdev.rest.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,9 @@ public class UserService {
 		return repo.findAll();
 	}
 
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
 }
