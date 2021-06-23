29f630ead4c196adad2227426d673d7ad8a85781
VictorW-dev <victorwinicius.bs@gmail.com>
2021-06-23 00:33:02 -0300

    Simple query with query methods

---
+++ src/main/java/com/br/vdev/rest/resources/util/URL.java
@@ -0,0 +1,16 @@
+package com.br.vdev.rest.resources.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class URL {
+	
+	public static String decodeParam(String text) {
+		try {
+			return URLDecoder.decode(text, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+
+}
