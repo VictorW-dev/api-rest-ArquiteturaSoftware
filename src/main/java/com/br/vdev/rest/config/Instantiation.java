770cbb3bceb7f410f3c0fc868b07729d223d53be
VictorW-dev <victorwinicius.bs@gmail.com>
2021-06-23 00:16:03 -0300

    Adding comments to posts

--- src/main/java/com/br/vdev/rest/config/Instantiation.java
+++ src/main/java/com/br/vdev/rest/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.br.vdev.rest.domain.Post;
 import com.br.vdev.rest.domain.User;
 import com.br.vdev.rest.dto.AuthorDTO;
+import com.br.vdev.rest.dto.CommentDTO;
 import com.br.vdev.rest.repository.PostRepository;
 import com.br.vdev.rest.repository.UserRepository;
 
@@ -41,6 +42,13 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("22/06/2021"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!",
 				new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/06/2021"), "Bom dia", "Hoje o dia está lindo!", new AuthorDTO(maria));
+
+		CommentDTO c1 = new CommentDTO("Boa viagem, mano!", sdf.parse("23/06/2021"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("23/06/2021"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/06/2021"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
