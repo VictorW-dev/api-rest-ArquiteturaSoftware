4c019d8c11518aecd8343fff97edeaf233ed7c26
VictorW-dev <victorwinicius.bs@gmail.com>
2021-06-22 23:28:46 -0300

    Projection of author data with DTO

--- src/main/java/com/br/vdev/rest/config/Instantiation.java
+++ src/main/java/com/br/vdev/rest/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.br.vdev.rest.domain.Post;
 import com.br.vdev.rest.domain.User;
+import com.br.vdev.rest.dto.AuthorDTO;
 import com.br.vdev.rest.repository.PostRepository;
 import com.br.vdev.rest.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("22/06/2021"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/06/2021"), "Bom dia", "Hoje o dia está lindo!", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("22/06/2021"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/06/2021"), "Bom dia", "Hoje o dia está lindo!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}
