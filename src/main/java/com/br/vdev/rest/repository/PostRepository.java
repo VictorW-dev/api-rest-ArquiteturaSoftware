d61f10e45902d2f8bc0e5b0a6aaafbdb21a1638f
VictorW-dev <victorwinicius.bs@gmail.com>
2021-06-23 01:34:51 -0300

    Query with multiple criteria

--- src/main/java/com/br/vdev/rest/repository/PostRepository.java
+++ src/main/java/com/br/vdev/rest/repository/PostRepository.java
@@ -1,9 +1,10 @@
 package com.br.vdev.rest.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-//import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.br.vdev.rest.domain.Post;
@@ -11,13 +12,19 @@ import com.br.vdev.rest.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
-	/* CONSULTA SIMPLES COM @QUERY
-	 * @Query("{ <field>: { $regex: /pattern/, $options: '<options>' } }")
-	 * @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
-	 * List<Post> findByTitle(String text);
-	*/
+	// CONSULTA SIMPLES COM @QUERY
+	// @Query("{ <field>: { $regex: /pattern/, $options: '<options>' } }")
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
 
 	// CONSULTA SIMPLES COM QUERY METHODS
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	// CONSULTA COM VÁRIOS CRITÉRIOS
+	// @Query({ $and: [ { <expression1> }, { <expression2> } , ... , { <expressionN> } ] })
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: {$lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
--- src/main/java/com/br/vdev/rest/services/PostService.java
+++ src/main/java/com/br/vdev/rest/services/PostService.java
@@ -1,5 +1,6 @@
 package com.br.vdev.rest.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,13 +22,20 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
-	public List<Post> findByTitle(String text){
+	public List<Post> findByTitle(String text) {
 		
 		// CONSULTA SIMPLES COM QUERY METHODS
 		return repo.findByTitleContainingIgnoreCase(text);
 		
 		// CONSULTA SIMPLES COM @QUERY
 		//return repo.findByTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 
 }
