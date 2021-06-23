8943ba77f489b382e1aad8fb7413a33ef13fd5f4
VictorW-dev <victorwinicius.bs@gmail.com>
2021-06-22 23:37:14 -0300

    Referencing user posts

--- src/main/java/com/br/vdev/rest/config/Instantiation.java
+++ src/main/java/com/br/vdev/rest/config/Instantiation.java
@@ -19,13 +19,13 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
@@ -35,13 +35,17 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-		
-		Post post1 = new Post(null, sdf.parse("22/06/2021"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+
+		Post post1 = new Post(null, sdf.parse("22/06/2021"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!",
+				new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/06/2021"), "Bom dia", "Hoje o dia está lindo!", new AuthorDTO(maria));
-		
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
+
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 
 	}
 
